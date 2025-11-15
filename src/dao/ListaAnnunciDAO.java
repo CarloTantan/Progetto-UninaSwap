85c23357e812384649d92efa8bc9e2324ea50361
CarloTantan <Carloarlistico60@gmail.com>
2025-11-15 18:49:05 +0100

    s

--- src/dao/ListaAnnunciDAO.java
+++ src/dao/ListaAnnunciDAO.java
@@ -253,7 +253,7 @@ public class ListaAnnunciDAO { //lista AnnunciDAO
 						rs.getString("idOggetto"),
 						TipologiaCategoria.valueOf(rs.getString("TipologiaCategoria")),  
 						rs.getDate("DataPubblicazione"), 
-						rs.getFloat("PrezzoVendita")
+						rs.getFloat("PrezzoVendita    ")
   
 						);
 				Annunci.add(annunciV);
