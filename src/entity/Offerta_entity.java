139ed293ca4f9cfcd06c5afa582d3b4dd5a0a446
Markis <markusdep005@gmail.com>
2025-12-01 18:15:44 +0100

    fix offerta

--- src/entity/Offerta_entity.java
+++ src/entity/Offerta_entity.java
@@ -6,25 +6,21 @@ public class Offerta_entity {
 	protected String MatricolaAcquirente;
 	protected String TipologiaOfferta ;
 	protected int IdAnnuncio;
-	protected int NumeroOfferte;
 	
 	
-	public Offerta_entity(int IdOfferta, String StatoOfferta, String MatricolaAcquirente, int IdAnnuncio,String TipologiaOfferta,int NumeroOfferte ) {
+	public Offerta_entity(int IdOfferta, String StatoOfferta, String MatricolaAcquirente, int IdAnnuncio,String TipologiaOfferta) {
 		this.IdOfferta = IdOfferta;
 		this.StatoOfferta = StatoOfferta;
 		this.MatricolaAcquirente = MatricolaAcquirente;
 		this.IdAnnuncio = IdAnnuncio;
 		this.TipologiaOfferta=TipologiaOfferta;
-		this.NumeroOfferte= NumeroOfferte;
 	}
 	
 //Getter	
 	public int getIdOfferta() {
 		return IdOfferta;
 	}
-	public int getNumeroOfferte() {
-		return NumeroOfferte;
-	}
+	
 	
 	public String getStatoOfferta() {
 		return StatoOfferta;
@@ -60,12 +56,6 @@ public class Offerta_entity {
 	public void setIdAnnuncio(int IdAnnuncio) {
 		this.IdAnnuncio = IdAnnuncio;
 	}
-	public void setNumeroOfferte(int NumeroOfferte) {
-		this.NumeroOfferte = NumeroOfferte;
-	}
-	
-	
-	
 	
 	
 }
